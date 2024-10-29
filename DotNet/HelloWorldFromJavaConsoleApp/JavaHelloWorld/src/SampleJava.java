6c82e8cda37e3e38e2362205772b22bb640c6a2f
Artur Fiedorowicz <afiedorowicz@javonet.com>
2024-10-29 18:56:40 +0100

    Hello World Console App - Calling Java from .Net

---
+++ DotNet/HelloWorldFromJavaConsoleApp/JavaHelloWorld/src/SampleJava.java
@@ -0,0 +1,11 @@
+public class SampleJava {
+    public static String HelloWorld()
+    {
+        return "Hello World from Java!";
+    }
+
+    public static Integer Multiply(Integer a, Integer b)
+	{
+		return a*b;
+	}
+}
---
+++ DotNet/HelloWorldFromJavaConsoleApp/NetCoreConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HelloJavonetWorld
+{
+    using Javonet.Netcore.Sdk;
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            // use Activate only once in your application
+            Javonet.Activate("Wy83-Kb36-Sx3t-Zp89-Yg82");
+
+            // create called runtime context
+            var javaRuntime = Javonet.InMemory().Jvm();
+
+            // load custom library
+            javaRuntime.LoadLibrary("../JavaHelloWorld/SampleJava.jar");
+
+            // get type from the runtime
+            var javaType = javaRuntime.GetType("SampleJava").Execute();
+
+            // invoke type's static method
+            var response = javaType.InvokeStaticMethod("multiplyByTwo", 2).Execute();
+
+            // get value from response
+            var result = (string)response.GetValue();
+
+            // write result to console
+            System.Console.WriteLine(result);
+
+            //Call Static Multiply Method
+            var multiplyResponse = javaType.InvokeStaticMethod("Multiply", 7, 7).Execute();
+
+            // get value from response
+            var multiplyResult = (int)multiplyResponse.GetValue();
+
+            // write result to console
+            System.Console.WriteLine(multiplyResult);
+        }
+    }
+}
